85235e4496eb1547c1706f45417ed881e8c62848
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-15 23:13:52 +0530

    added generation.js

---
+++ generation.js
@@ -0,0 +1,34 @@
+const gen = {
+	one: {
+		start: 1,
+		end: 151,
+	},
+	two: {
+		start: 152,
+		end: 251,
+	},
+	three: {
+		start: 252,
+		end: 386,
+	},
+	four: {
+		start: 387,
+		end: 493,
+	},
+	five: {
+		start: 494,
+		end: 649,
+	},
+	six: {
+		start: 650,
+		end: 721,
+	},
+	seven: {
+		start: 722,
+		end: 809,
+	},
+	eight: {
+		start: 810,
+		end: 905,
+	},
+}
