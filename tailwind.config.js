0aff25bed284007643eb68183dff115fca758721
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-15 14:08:56 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['*html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Geist', 'sans-serif'],
+				chelsea: ['Chelsea', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [require('daisyui')],
+}
